64b036274b4ed71c4216fb088bc0a6ada1bd9be7
CalebJEJ <cjj226@nau.edu>
2020-02-06 15:42:12 -0700

    Transformed detect_video
    Another file transofrmeed to make calling it easier from a function that will import it later

--- Tensorflow2.0-Workbench/detect_video.py
+++ Tensorflow2.0-Workbench/detect_video.py
@@ -21,39 +21,38 @@ flags.DEFINE_string('output', None, 'path to output video')
 flags.DEFINE_string('output_format', 'XVID', 'codec used in VideoWriter when saving video to file')
 flags.DEFINE_integer('num_classes', 80, 'number of classes in the model')
 
-
-def main(_argv):
+def run_detect_video(classes, weights, tiny, size, video, output, output_format, num_classes):
     physical_devices = tf.config.experimental.list_physical_devices('GPU')
     if len(physical_devices) > 0:
         tf.config.experimental.set_memory_growth(physical_devices[0], True)
 
-    if FLAGS.tiny:
-        yolo = YoloV3Tiny(classes=FLAGS.num_classes)
+    if tiny:
+        yolo = YoloV3Tiny(classes=num_classes)
     else:
-        yolo = YoloV3(classes=FLAGS.num_classes)
+        yolo = YoloV3(classes=num_classes)
 
-    yolo.load_weights(FLAGS.weights)
+    yolo.load_weights(weights)
     logging.info('weights loaded')
 
-    class_names = [c.strip() for c in open(FLAGS.classes).readlines()]
+    class_names = [c.strip() for c in open(classes).readlines()]
     logging.info('classes loaded')
 
     times = []
 
     try:
-        vid = cv2.VideoCapture(int(FLAGS.video))
+        vid = cv2.VideoCapture(int(video))
     except:
-        vid = cv2.VideoCapture(FLAGS.video)
+        vid = cv2.VideoCapture(video)
 
     out = None
 
-    if FLAGS.output:
+    if output:
         # by default VideoCapture returns float instead of int
         width = int(vid.get(cv2.CAP_PROP_FRAME_WIDTH))
         height = int(vid.get(cv2.CAP_PROP_FRAME_HEIGHT))
         fps = int(vid.get(cv2.CAP_PROP_FPS))
-        codec = cv2.VideoWriter_fourcc(*FLAGS.output_format)
-        out = cv2.VideoWriter(FLAGS.output, codec, fps, (width, height))
+        codec = cv2.VideoWriter_fourcc(*output_format)
+        out = cv2.VideoWriter(output, codec, fps, (width, height))
 
     while True:
         _, img = vid.read()
@@ -63,9 +62,9 @@ def main(_argv):
             time.sleep(0.1)
             continue
 
-        img_in = cv2.cvtColor(img, cv2.COLOR_BGR2RGB) 
+        img_in = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         img_in = tf.expand_dims(img_in, 0)
-        img_in = transform_images(img_in, FLAGS.size)
+        img_in = transform_images(img_in, size)
 
         t1 = time.time()
         boxes, scores, classes, nums = yolo.predict(img_in)
@@ -76,13 +75,19 @@ def main(_argv):
         img = draw_outputs(img, (boxes, scores, classes, nums), class_names)
         img = cv2.putText(img, "Time: {:.2f}ms".format(sum(times)/len(times)*1000), (0, 30),
                           cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (0, 0, 255), 2)
-        if FLAGS.output:
+        if output:
             out.write(img)
         cv2.imshow('output', img)
         if cv2.waitKey(1) == ord('q'):
             break
 
     cv2.destroyAllWindows()
+
+
+def main(_argv):
+    run_detect_video(FLAGS.classes, FLAGS.weights, FLAGS.tiny,
+                     FLAGS.size, FLAGS.video, FLAGS.output,
+                     FLAGS.output_format, FLAGS.num_classes)
 
 
 if __name__ == '__main__':
