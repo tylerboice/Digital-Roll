8213ccfd4488441b8839fcbae03b962bbee7e226
CalebJEJ <cjj226@nau.edu>
2020-02-16 20:35:41 -0700

    Updated run_workbecnh with functioning load
    load option is now fully operational

--- Tensorflow2.0-Workbench/run_workbench.py
+++ Tensorflow2.0-Workbench/run_workbench.py
@@ -168,57 +168,82 @@ def main():
                 pref_path = userInput[2:]
             else:
                 pref_path = userInput[5:]
+            pref_path.rstrip("\n\r")
             print("Searching for: " + pref_path)
             if path.exists(os.getcwd() + "/" + pref_path):
                 pref_path = os.getcwd() + "/" + pref_path
                 print("Found file at: " + pref_path)
             if path.exists(pref_path):
-                #TODO Set new preferences
+                # Set new preferences
                 preferences.pref_file = pref_path
-                defaults.DEFAULT_PREF_PATH = pref_path
-                preferences.batch_size = defaults.check_preferences(defaults.BATCH_SIZE_VAR,
-                                                        defaults.FLAGS.batch_size,
-                                                        defaults.INT)
-                preferences.checkpoint_output = defaults.check_preferences(defaults.CHECKPOINT_VAR,
-                                                               defaults.FLAGS.checkpoint_path,
-                                                               defaults.FILE)
-                preferences.classifier_file = defaults.check_preferences(defaults.CLASSIFIERS_VAR,
-                                                             defaults.FLAGS.classifiers,
-                                                             defaults.FILE)
-                preferences.dataset_test = defaults.check_preferences(defaults.DATASET_TEST_VAR,
-                                                                      defaults.FLAGS.dataset_test,
-                                                                      defaults.FILE)
-                preferences.dataset_train = defaults.check_preferences(defaults.DATASET_TRAIN_VAR,
-                                                                       defaults.FLAGS.dataset_train,
-                                                                       defaults.FILE)
-                preferences.epochs = defaults.check_preferences(defaults.EPOCH_NUM_VAR,
-                                                                defaults.FLAGS.epochs,
-                                                                defaults.INT)
-                preferences.image_size = defaults.check_preferences(defaults.IMAGE_SIZE_VAR,
-                                                                    defaults.FLAGS.image_size,
-                                                                    defaults.INT)
-                preferences.learning_rate = defaults.check_preferences(defaults.LEARN_RATE_VAR,
-                                                                       defaults.FLAGS.learn_rate,
-                                                                       defaults.FLOAT)
-                preferences.mode = defaults.check_preferences(defaults.MODE_VAR,
-                                                              defaults.FLAGS.mode,
-                                                              defaults.MODE_OPTIONS)
-                preferences.num_classes = files.get_num_classes(preferences.classifier_file)
-                preferences.output = defaults.check_preferences(defaults.OUTPUT_VAR,
-                                                                defaults.FLAGS.output,
-                                                                defaults.FILE)
-                preferences.tiny = defaults.check_preferences(defaults.TINY_WEIGHTS_VAR,
-                                                              defaults.FLAGS.tiny_weights,
-                                                              defaults.BOOL)
-                preferences.transfer = defaults.check_preferences(defaults.TRANSFER_VAR,
-                                                                  defaults.FLAGS.transfer,
-                                                                  defaults.TRANSFER_OPTIONS)
-                preferences.validate_input = defaults.check_preferences(defaults.VALID_IN_VAR,
-                                                                        defaults.FLAGS.validate_image_input,
-                                                                        defaults.FILE)
-                preferences.weight_num_classes = defaults.check_preferences(defaults.WEIGHTS_CLASS_VAR,
-                                                                            defaults.FLAGS.weights_path,
-                                                                            defaults.INT)
+                with open(pref_path, "r") as f:
+                    for line in f.readlines():
+                        if defaults.BATCH_SIZE_VAR + ":" in line:
+                            txt_input = line.split(":")[1]
+                            preferences.batch_size = txt_input
+
+                        elif defaults.CHECKPOINT_VAR + ":" in line:
+                            txt_input = line.split(":")[1]
+                            preferences.checkpoint_output = txt_input
+
+                        elif defaults.CLASSIFIERS_VAR + ":" in line:
+                            txt_input = line.split(":")[1]
+                            txt_input = txt_input.rstrip()
+                            preferences.classifier_file = txt_input
+                            try:
+                                preferences.num_classes = files.get_num_classes(preferences.classifier_file)
+                            except:
+                                try:
+                                    preferences.num_classes = files.get_num_classes(os.getcwd()
+                                                                                    + "/"
+                                                                                    + preferences.classifier_file[1:])
+                                except:
+                                    print("ERROR: Failed to update classifier file, new file not found")
+
+                        elif defaults.DATASET_TEST_VAR + ":" in line:
+                            txt_input = line.split(":")[1]
+                            preferences.dataset_test = txt_input
+
+                        elif defaults.DATASET_TRAIN_VAR + ":" in line:
+                            txt_input = line.split(":")[1]
+                            preferences.dataset_train = txt_input
+
+                        elif defaults.EPOCH_NUM_VAR + ":" in line:
+                            txt_input = line.split(":")[1]
+                            preferences.epochs = txt_input
+
+                        elif defaults.IMAGE_SIZE_VAR + ":" in line:
+                            txt_input = line.split(":")[1]
+                            preferences.image_size = txt_input
+
+                        elif defaults.LEARN_RATE_VAR + ":" in line:
+                            txt_input = line.split(":")[1]
+                            preferences.learning_rate = txt_input
+
+                        elif defaults.MODE_VAR + ":" in line:
+                            txt_input = line.split(":")[1]
+                            preferences.mode = txt_input
+
+                        elif defaults.OUTPUT_VAR + ":" in line:
+                            txt_input = line.split(":")[1]
+                            preferences.output = txt_input
+
+                        elif defaults.TINY_WEIGHTS_VAR + ":" in line:
+                            txt_input = line.split(":")[1]
+                            preferences.tiny = txt_input
+
+                        elif defaults.TRANSFER_VAR + ":" in line:
+                            txt_input = line.split(":")[1]
+                            preferences.transfer = txt_input
+
+                        elif defaults.VALID_IN_VAR + ":" in line:
+                            txt_input = line.split(":")[1]
+                            preferences.validate_input = txt_input
+
+                        elif defaults.WEIGHTS_CLASS_VAR + ":" in line:
+                            txt_input = line.split(":")[1]
+                            preferences.weight_num_classes = txt_input
+
                 print("\nNew Preferences:")
                 preferences.print_pref()
             else:
