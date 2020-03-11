c5959075db86e060ec9d8b932a28dca29eeda043
CalebJEJ <cjj226@nau.edu>
2020-03-10 17:29:56 -0700

    Reverted coreML
    left in a comment at most, issue is with the input_name_shape_dict variable in the converter call

--- Tensorflow2.0-Workbench/scripts/create_coreml.py
+++ Tensorflow2.0-Workbench/scripts/create_coreml.py
@@ -1,15 +1,17 @@
 from tensorflow.keras.applications import MobileNet
 import tfcoreml
-from scripts import preferences
+#from scripts import preferences
 
 def export_coreml(output):
+    #input_shape_dict =
     keras_model = MobileNet(weights=None, input_shape=(224, 224, 3))
     keras_model.save(output, save_format='tf')
     # tf.saved_model.save(keras_model, './savedmodel')
-    model = tfcoreml.convert(output,
-                             mlmodel_path = output + "coreML_model.mlmodel",
+    model = tfcoreml.convert(tf_model_path=output,
+                             mlmodel_path=output + "coreML_model.mlmodel",
                              input_name_shape_dict={'input_1': (1, 224, 224, 3),
                                                     'input_55': (1, 224, 224, 3),
                                                     'input_28': (1, 224, 224, 3)},
                              output_feature_names=['Identity'],
                              minimum_ios_deployment_target='13')
+
